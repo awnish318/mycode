4a195208bba12e113c5a05419d54d7d0147b470a
awnish318 <83909285+awnish318@users.noreply.github.com>
2021-11-05 13:00:19 +0530

    Add files via upload
    program for accepting and displaying array using pointers..

---
+++ accepting and displaying array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void accept(int *p,int*q){
+    scanf("%d",q);
+    for(int i=0;i<(*q);i++{
+        scanf("%d",&p[i]);
+    }
+    coid display(int *a,int n){
+        for (int i=0;i<n;i++){
+            printf("%d",a[i]);
+        }
+    }
+}
+int main() {
+
+int a[20];
+int n;
+accept(a,&n);
+display (a,n);   
+}
